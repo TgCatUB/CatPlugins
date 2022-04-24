39195fcc0129a686ea12f808ba544d0ed3841a88
Jisan <66010486+Jisan09@users.noreply.github.com>
2022-04-24 06:19:28 +0530

    Update status.py

--- status.py
+++ status.py
@@ -45,6 +45,7 @@ async def pussy(event):
     os.remove(photo)
     first_name = user.first_name
     addgvar("my_first_name", first_name)
+    addgvar("my_last_name", "")
     last_name = user.last_name
     if last_name:
         addgvar("my_last_name", last_name)
